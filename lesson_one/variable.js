10c67bc8e576b7064d59a10a0526fc6126f6c308
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2023-12-04 15:17:27 +0000

    comit

---
+++ lesson_one/variable.js
@@ -0,0 +1,17 @@
+// var , let , const
+
+const accountId = 1345748
+let accountEmail = "das@pujon.com"
+var accountPass = 12345
+accountCity = "Chattogram"
+
+
+// accountId = 65665
+accountEmail = "snigda@pujon.com"
+accountPass = 65658
+accountCity = "Dhaka"
+
+
+console.log();
+
+console.table([accountId, accountEmail, accountPass, accountCity])
