bcb9c68540e1941d5793910ca035e3c675f3b341
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2023-12-22 06:34:32 +0000

    change

---
+++ jsBasics/variable.js
@@ -0,0 +1,74 @@
+// var , let , const
+
+// const accountId = 1345748
+// let accountEmail = "das@pujon.com"
+// var accountPass = 12345
+// accountCity = "Chattogram"
+
+
+// // accountId = 65665
+// accountEmail = "snigda@pujon.com"
+// accountPass = 65658
+// accountCity = "Dhaka"
+
+
+// console.log();
+
+// console.table([accountId, accountEmail, accountPass, accountCity])
+
+
+// const profile = {
+//     profileName : "Pujon Das",
+//     posts : 195,
+//     followers : 495,
+//     following : 5,
+//     isFollow :  false
+// }
+
+// console.log(typeof profile["following"])
+// console.log(typeof profile)
+
+// function show() {
+//    {
+//     (function(){
+//         var x = 5;
+//         var y = 6;
+//     })();
+//    }
+//     console.log(x,y)
+// }
+
+// show()
+
+// if(true) {
+//     var a = 5;
+//     var a = 7;
+// }
+
+// console.log(a)
+
+// if(true) {
+//     let b = 5;
+//     b = 8;
+// console.log(b)
+
+// }
+
+
+// console.log(2 === 2)
+// console.log("2" === 2)
+
+// console.log(null > 0)
+
+// function sum(n1, n2){
+//     return n1+n2;
+// } 
+
+// console.log(typeof sum)
+
+
+const strNum = '3';
+
+const number = Number(strNum)
+
+console.log(number)
