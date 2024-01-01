a5b914eb2ad3767cf803118caddaf733fbaf43f3
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2024-01-01 13:34:59 +0000

    date

--- jsBasics/variable.js
+++ jsBasics/variable.js
@@ -76,23 +76,68 @@
 // stack and heap memory
 // Stack (Primitive) Heap (Non-Primitive)
 
-let myCompany = 'Data Verify'
+// let myCompany = 'Data Verify'
 
-anotherCompany = myCompany
+// anotherCompany = myCompany
 
-anotherCompany = 'Data Temple'
+// anotherCompany = 'Data Temple'
 
-console.log(myCompany)
-console.log(anotherCompany)
+// console.log(myCompany)
+// console.log(anotherCompany)
 
 
-let operatorOne = {
-    id : 'OPRON203',
-    name : 'Pujon Das'
-}
+// let operatorOne = {
+//     id : 'OPRON203',
+//     name : 'Pujon Das'
+// }
 
-let operatorTwo = operatorOne
+// let operatorTwo = operatorOne
 
-operatorTwo.id = 'BG01017'
+// operatorTwo.id = 'BG01017'
 
-console.log(operatorOne)
+// console.log(operatorOne)
+
+// let name = 'pujon'
+// let repo = 40;                                                       
+
+// console.log(name)
+
+// const salary = 8000;
+
+// console.log(salary)
+
+// let newSalary = new Number(100000);
+
+// console.log(newSalary.toLocaleString("en-IN"))
+// console.log(newSalary)
+
+// abs, ceil, floor, max, min, pow, random, round, sqrt
+
+// console.log(Math.abs(-4.5))
+// console.log(Math.round(4.7))
+// console.log(Math.ceil(5.1))
+// console.log(Math.floor(5.8))
+// console.log(Math.pow(5, 2))
+// console.log(Math.sqrt(64))
+// console.log(Math.min(5, 25, 36, 17))
+// console.log(Math.max(5, 25, 36, 17))
+// console.log(Math.random(1,7).toFixed())
+
+// date
+
+const myDate = new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate);
+
+// const anotherDate = Date.now();
+
+// console.log(anotherDate)
+
+const pritiDate = new Date("2023", "02", "14", "5", "12", "30");
+
+console.log(pritiDate.getMonth());
