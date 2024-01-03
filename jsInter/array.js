ea86a5d1dba2831fd03dca2349aa5c7e51e6c87e
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2024-01-03 14:20:14 +0000

    arrayUpdate

--- jsInter/array.js
+++ jsInter/array.js
@@ -1,3 +1,36 @@
-const myarr = [1,2,3,4,5]
+// const myarr = [1,2,3,'pujon', true]
+// const myArr1 = ['priti', 'susmoy', 'niloy', 'rinky', 'jhorna']
+// myarr.push('priti', 0)
+// myarr.pop()
+// myarr.unshift('niloy', 'rinky')
+// myarr.shift()
+// myarr.sort()
+// const sliceArr = myarr.slice(3,5)
+// const spliceArr = myarr.splice(3,5)
+// console.log(myarr)
+// console.log(spliceArr)
 
-console.log(myarr)
+// const myArr2 = myarr.reduce()
+// console.log(myArr2)
+
+const teacher = ['Priti', 'Pujon', 'Sajal', 'Topu']
+const student = ['Susmoy', 'Rohan', 'Niloy', 'Rinky']
+const together = ['Priti', 'Topu', ['Niloy', 'Rinky'], 'Pujon', ['Sajal', 'Rohan', ['Susmoy']]]
+
+// teacher.push(student)
+// const ts = teacher.concat(student)
+
+// const tS = [...teacher, ...student]
+// const togDef = together.flat(Infinity)
+
+// console.log(togDef);
+
+// console.log(Array.isArray('Priti'));
+// console.log(Array.from('Priti'));
+// console.log(Array.from({name: 'pollob', id: 50}));
+
+const firstTeacher = 'Priti'
+const secondTeacher = 'topu'
+const thirdTeacher = 'sajal'
+
+console.log(Array.of(firstTeacher,secondTeacher,thirdTeacher));
