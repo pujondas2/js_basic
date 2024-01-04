7f0d69698a6ce2ce3913d39bc3fdfb07f6b1c4dd
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2024-01-04 16:16:15 +0000

    objectUpdate

--- jsInter/object.js
+++ jsInter/object.js
@@ -26,12 +26,69 @@ user.greeting = function(){
     console.log('Happy New Year')
 }
 
-console.log(user.greeting())
+// console.log(user.greeting())
 
 user.greetingPriti = function(){
     console.log(`Happy New Year, ${this.guest} Madam`)
 }
 
-console.log(user.greetingPriti());
+// console.log(user.greetingPriti());
 
-console.log(user);
+// console.log(user);
+
+
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.userName = 'pritichy';
+tinderUser.password = 'chy01234';
+tinderUser.isLoggedIn = false;
+tinderUser.followers = 10;
+
+// console.log(tinderUser);
+
+const finderUser = {
+    user : {
+        username : {
+            fullname : {
+                firstName : 'Priti',
+                lastName : 'Choudhary'
+            }
+        }
+    }
+}
+
+// console.log(finderUser.user.username.fullname.firstName);
+
+const teacher1 = {pname: "priti", pid: "10"}
+const teacher2 = {tname: "topu", tid: "13"}
+const teacher3 = {sname: "sajal", sid: "8"}
+
+// const teachers = Object.assign({}, teacher1, teacher2, teacher3)
+
+const teachers = {...teacher1, ...teacher2, ...teacher3}
+
+// console.log(teachers);
+
+const person = [
+    {
+        id : 1,
+        name : "Priti"
+    },
+    {
+        id : 2,
+        name : "Topu"
+    },
+    {
+        id : 3,
+        name : "Sajal"
+    }
+]
+
+// console.log(person[1].name)
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+// console.log(tinderUser.hasOwnProperty('isLogged'))
