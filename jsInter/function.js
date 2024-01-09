a7affb2541ec56864a6f2e65803e0450bb319788
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2024-01-09 15:49:57 +0000

    funcArrObj

--- jsInter/function.js
+++ jsInter/function.js
@@ -25,4 +25,34 @@ function loginMsg(username = 'priti00') {
     }
 }
 
-console.log(loginMsg())
+// console.log(loginMsg())
+
+function addToCart(cart1, cart2, ...cartall) {
+    return [cart1, cart2, cartall]
+}
+
+// console.log(addToCart(200, 400, 300, 1200, 180, 560));
+
+const madam = {
+    name : 'Priti',
+    age : 26
+}
+
+function handleObject(getObject) {
+    return `Her name is ${getObject.name} and her age is ${getObject.age}`
+}
+
+// console.log(handleObject(madam))
+// console.log(handleObject({
+//     name : 'Rinky',
+//     age: 29
+// }))
+
+const price = [500, 250, 360, 240]
+
+function handleArray(getArray) {
+    return `Shirt is ${getArray[2]} TK,\nT-Shirt is ${getArray[1]} TK.`
+}
+
+// console.log(handleArray(price));
+console.log(handleArray([850, 630, 870]));
