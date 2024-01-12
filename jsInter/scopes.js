47a77a961aa083a474ebbfbfec300948f3e0589e
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2024-01-12 04:50:33 +0000

    scopelvl&miniHoisting

---
+++ jsInter/scopes.js
@@ -0,0 +1,53 @@
+var a = 100
+let b = 11
+
+
+if (true){
+    var a = 3330;
+    let b = 20; 
+    const c = 30;
+    // console.log(b)
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = 'pujon'
+    function two(){
+        const website = 'github'
+        // console.log(username)
+    }
+
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if (true) {
+    const username = 'sushmoy'
+    if (username === 'sushmoy') {
+        const website = 'skool.com'
+        // console.log(username)
+    }
+    // console.log(website);
+}
+
+// ++++++++++++++++++++++++++++++
+
+addTwo(5)
+function addTwo(num) {
+    console.log(num + 2)
+}
+
+
+
+addFive(5)
+const addFive = function(num) {
+    console.log(num + 5);
+}
