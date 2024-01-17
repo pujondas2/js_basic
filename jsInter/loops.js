e81c11c402db0667da590d4cf5e87096d0ef7981
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2024-01-17 15:24:19 +0000

    dowhile

--- jsInter/loops.js
+++ jsInter/loops.js
@@ -35,8 +35,50 @@ for (let i = 1; i <= 10; i++) {
 for (let i = 1; i <= 10; i++) {
     const element = i;
     if (element == 5) {
-        console.log(`Dedicated 5`);
+        // console.log(`Dedicated 5`);
         continue;
     }
-    console.log(element)
+    // console.log(element)
 }
+
+let index = 1
+while (index <= 10) {
+    // console.log(`${index}`);
+    index = index + 2;
+}
+
+const instructor = ['Priti', 'Topu', 'Niloy', 'Rinky']
+
+let inst = 0;
+while(inst < instructor.length){
+    // console.log(instructor[inst]);
+    inst = inst + 1
+}
+
+
+let table = 1;
+while (table <= 5) {
+    table1 = 1;
+    while(table1 <= 10){
+        // console.log(`${table} * ${table1} = ` + table*table1);
+        table1++;
+    }
+    table++;
+}
+
+let num = 1;
+do {
+    // console.log(num);
+    num++;
+} while (num < 5);
+
+
+let i = 1;
+do {
+    let j = 1;
+    do {
+        console.log(`${i} * ${j} = ` + i*j);
+        j++
+    } while (j <= 10);
+    i++;
+} while (i <= 5);
